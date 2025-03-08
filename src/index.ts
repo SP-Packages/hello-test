0bcec14995a9f3a1d212104c10979745c63feea5
Senthilprabu Ponnusamy <p.senthilprabu@gmail.com>
2025-03-07 21:22:26 -0500

    feat(lint): add linting to the project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-console */
 
 /**
  * This is a simple function that logs "Hello, Test!" to the console.
  */
-export function helloTest() {
+export function helloTest(): void {
   console.log("Hello, Test!");
 }
